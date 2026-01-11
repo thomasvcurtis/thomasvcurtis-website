2b52291cd215a9f48d9d4bcb62beffce13ee9661
thomasvcurtis <thomascurtisengineer@gmail.com>
2026-01-11 14:21:00 -0500

    chore: enhance Vite configuration with build options for output directory and assets

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/thomasvcurtis-website/'
+  base: '/thomasvcurtis-website/',
+  build: {
+    sourcemap: false,
+    outDir: 'dist',
+    assetsDir: 'assets',
+  },
 })
